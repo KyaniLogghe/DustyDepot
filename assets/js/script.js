153f66a8db7a3cf520a652652d484906adde1c19
LikeAaron <aaron.vanmarcke@student.howest.be>
2022-12-21 12:00:37 +0100

    added form

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,9 +29,39 @@ function infoPopµp(e) {
   document.querySelector(".pop-up").classList.toggle("hidden");
   let popup = document.querySelector(".pop-up div");
   popup.innerHTML = "";
+  //inserting the job form
+
   popup.insertAdjacentHTML(
     "afterbegin",
-    `<h3>Info</h3> <p>Here is some info about ${job}</p>`
+    `
+    <div class="rendered-form">
+    <div class="">
+        <h3>Info</h3> <p>Here is some info about ${job}</p></div>
+    <div class="formbuilder-text form-group field-text-1671619917753">
+        <label for="text-1671619917753" class="formbuilder-text-label">Name</label>
+        <input type="text" placeholder="John Doe" name="text-1671619917753" access="false" maxlength="20" id="text-1671619917753">
+    </div>
+    <div class="formbuilder-text form-group field-text-1671620106132">
+        <label for="text-1671620106132" class="formbuilder-text-label">e-mail</label>
+        <input type="text" placeholder="john.doe@dustydepot.be" name="text-1671620106132" access="false" id="text-1671620106132">
+    </div>
+    <div class="formbuilder-date form-group field-date-1671620155874">
+        <label for="date-1671620155874" class="formbuilder-date-label">birthday</label>
+        <input type="date" name="date-1671620155874" access="false" id="date-1671620155874">
+    </div>
+    <div class="formbuilder-textarea form-group field-textarea-1671620017313">
+        <label for="textarea-1671620017313" class="formbuilder-textarea-label">motivation letter</label>
+        <textarea type="textarea" class="form-control" name="textarea-1671620017313" access="false" id="textarea-1671620017313"></textarea>
+    </div>
+    <div class="formbuilder-file form-group field-file-1671619950969">
+        <label for="file-1671619950969" class="formbuilder-file-label">Upload resume</label>
+        <input type="file" class="form-control" name="file-1671619950969" access="false" multiple="false" id="file-1671619950969">
+    </div>
+    <div class="formbuilder-button form-group field-button-1671620059122">
+        <button type="button" class="btn-default btn" name="button-1671620059122" access="false" style="default" id="button-1671620059122">Submit</button>
+    </div>
+</div>
+    `
   );
   document.querySelector("#close").addEventListener("click", closePopµp);
 }
